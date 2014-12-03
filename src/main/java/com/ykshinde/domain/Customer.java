d895e356a66b461f5fd4a2eaa4d06e65964e7896
unknown <ys00770@WPUYS00770L.jdnet.deere.com>
2014-12-03 23:02:47 +0530

    Sample for Spring Boot MVC, Device Detection, WURFL

---
+++ src/main/java/com/ykshinde/domain/Customer.java
@@ -0,0 +1,31 @@
+package com.ykshinde.domain;
+
+public class Customer {
+	private String name;
+	
+	private String city;
+	
+	public Customer() {
+	}
+	
+	public Customer(String name, String city) {
+		this.name = name;
+		this.city = city;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
